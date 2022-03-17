355d777dcb924f5b20ce7e312111ae8c6fa2a00e
Brian <96350390+Qiick@users.noreply.github.com>
2022-03-17 11:45:44 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'NoobHosting Kennisbank',
+  tagline: '',
+  url: 'https://kb.noobhosting.net',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Qiick', // Usually your GitHub org/user name.
+  projectName: 'NoobDocsV2', // Usually your repo name.
 
   presets: [
     [
